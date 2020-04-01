6871c32010fc782272114c5d9325d292b47fe1b9
aicha <ai.elabdellaoui@gmail.com>
2020-04-01 20:03:49 +0200

    hh

--- src/app/controller/model/achatdetail.ts
+++ src/app/controller/model/achatdetail.ts
@@ -2,15 +2,15 @@ import {Livre} from './livre';
 import {Achat} from './achat';
 
 export class Achatdtail {
+  public  id: number;
   public prixUnitaire : number;
   public qte :number;
   public  livre : Livre;
-  public achat:Achat;
+
   constructor() {
     this.qte=0;
     this.prixUnitaire=0;
     this.livre= new Livre();
-    this.achat = new Achat();
   }
 
 }
